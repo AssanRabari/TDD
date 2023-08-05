f59829f71da92d66f6d06bdd0001dd35bfd3ff49
Assan Rabari <111674374+AssanRabari@users.noreply.github.com>
2023-08-05 13:32:09 +0530

    test case 2 with 8 chr 8 number of length 8

--- passwordValidator/passwordValidatorTest.test.js
+++ passwordValidator/passwordValidatorTest.test.js
@@ -4,4 +4,10 @@ describe("Password validator", () => {
     test('Empty string', () => {
         expect(validator("")).toBe(false);
     });
+
+    test('return true given a password 8 chr or longer, a letter, a number', () => {
+        expect(validator("1234567a")).toBe(true);
+        expect(validator("1234567A")).toBe(true);
+        expect(validator("12asdas567A")).toBe(true);
+    });
 });
