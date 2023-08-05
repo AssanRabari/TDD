0cde96d779142bb6113c2c6728af3edda16f29c8
Assan Rabari <111674374+AssanRabari@users.noreply.github.com>
2023-08-05 13:21:09 +0530

    test case 2 with multiple numbers

--- addition/additionCalculator.test.js
+++ addition/additionCalculator.test.js
@@ -4,4 +4,9 @@ describe("additionCalculator's test cases", () => {
   test("addition of 4 and 6 to equal 10", () => {
     expect(additionCalculator(4, 6)).toBe(10);
   });
+
+  test("addition of 100, 50, 20, 45 and 30 to equal 245", () => {
+    expect(additionCalculator(100, 50, 20, 45, 30)).toBe(245);
+  });
+  
 });
