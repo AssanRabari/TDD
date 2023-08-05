398bc6635e3e8f43fba9294fcd926aedbc18261c
Assan Rabari <111674374+AssanRabari@users.noreply.github.com>
2023-08-05 13:42:58 +0530

    fizz buzz test 2 returns fizz for numbers with multiply of 3

--- fizzBuzz/fizzBuzz.test.js
+++ fizzBuzz/fizzBuzz.test.js
@@ -4,4 +4,13 @@ describe("Fzz buzz", () => {
   test("returns 'zero' for input of 0", () => {
     expect(fizzBuzz(0)).toBe("zero");
   });
+
+  test("returns fizz for numbers with multiply of 3", () => {
+    expect(fizzBuzz(3)).toBe("fizz");
+    expect(fizzBuzz(18)).toBe("fizz");
+    expect(fizzBuzz(24)).toBe("fizz");
+    expect(fizzBuzz(48)).toBe("fizz");
+    expect(fizzBuzz(93)).toBe("fizz");
+    expect(fizzBuzz(1236)).toBe("fizz");
+  });
 });
